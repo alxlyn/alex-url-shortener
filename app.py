8ac8a6f0ac70f58e0b8e993ce15dd8c73c7387b0
Alex <alxlyan@Alexs-MacBook-Air.local>
2026-02-21 16:13:11 -0500

    Add redirect route using in memory URL map

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import string
 import secrets
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+url_map = {}
 
 @app.route("/")
 def home():
@@ -12,8 +13,16 @@ def home():
 def shorten():
     long_url = request.form.get("long_url", "").strip()
     code = generate_code()
+    url_map[code] = long_url
     short_url = request.host_url + code
     return render_template("index.html", short_url=short_url, long_url=long_url)
+
+@app.route("/<code>")
+def redirect_to_url(code):
+    long_url = url_map.get(code)
+    if long_url:
+        return redirect(long_url)
+    return "URL not found", 404
 
 def generate_code(length=6):
     alphabet = string.ascii_letters + string.digits
