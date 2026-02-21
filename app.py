9ac291c1aa4e69dd192690ee74c8b1d62a4e5858
Alex <alxlyan@Alexs-MacBook-Air.local>
2026-02-21 15:58:26 -0500

    Add URL submit form and /shorten POST route

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/shorten", methods=["POST"])
+def shorten():
+    long_url = request.form.get("long_url", "").strip()
+    return f"Got it: {long_url}"
 
 if __name__ == "__main__":
     app.run(debug=True)
